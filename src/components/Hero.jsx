dde14634a52f6a523d7c7408fc51764c86356979
vanshaj932 <karnvanshaj123@gmail.com>
2024-09-21 12:52:20 +0530

    First Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,196 @@
+import { curve, heroBackground, robot } from "../assets";
+import Button from "./Button";
+import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
+
+const Hero = () => {
+  const parallaxRef = useRef(null);
+
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative" ref={parallaxRef}>
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6 ">
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with{" "}
+            <span className="inline-block relative">
+              Brainwave{" "}
+              <img
+                src={curve}
+                alt="curve"
+                className="absolute top-full left-0 w-full xl:mt-2"
+                width={624}
+                height={28}
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href="/pricing" white>
+            Get Started
+          </Button>
+        </div>
+
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24 ">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl lg:flex ">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} alt={icon} width={24} height={25} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] lg:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
+              </div>
+            </div>
+
+            <Gradient />
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              alt="hero"
+              className="w-full"
+              width={1440}
+              height={1800}
+            />
+          </div>
+          <BackgroundCircles />
+        </div>
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
+      </div>
+
+      <BottomLine />
+    </Section>
+  );
+};
+
+export default Hero;
+
+// import { useLocation } from "react-router-dom";
+// import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+// import { brainwave } from "../assets";
+// import { navigation } from "../constants";
+// import Button from "./Button";
+// import MenuSvg from "../assets/svg/MenuSvg";
+// import { HamburgerMenu } from "./design/Header";
+// import { useState } from "react";
+
+// const Header = () => {
+//   const pathname = useLocation();
+//   const [openNavigation, setOpenNavigation] = useState(false);
+
+//   const toggleNavigation = () => {
+//     if (openNavigation) {
+//       setOpenNavigation(false);
+//       enablePageScroll();
+//     } else {
+//       setOpenNavigation(true);
+//       disablePageScroll();
+//     }
+//   };
+
+//   const handleClick = () => {
+//     if (!openNavigation) return;
+
+//     enablePageScroll();
+//     setOpenNavigation(false);
+//   };
+
+//   return (
+//     <div
+//       className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+//         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+//       }`}
+//     >
+//       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+//         <a className="block w-[12rem] xl:mr-8" href="#hero">
+//           <img src={brainwave} width={190} height={40} alt="Brainwave" />
+//         </a>
+
+//         <nav
+//           className={`${
+//             openNavigation ? "flex" : "hidden"
+//           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+//         >
+//           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+//             {navigation.map((item) => (
+//               <a
+//                 key={item.id}
+//                 href={item.url}
+//                 onClick={handleClick}
+//                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+//                   item.onlyMobile ? "lg:hidden" : ""
+//                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+//                   item.url === pathname.hash
+//                     ? "z-2 lg:text-n-1"
+//                     : "lg:text-n-1/50"
+//                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+//               >
+//                 {item.title}
+//               </a>
+//             ))}
+//           </div>
+
+//           <HamburgerMenu />
+//         </nav>
+
+//         <a
+//           href="#signup"
+//           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+//         >
+//           New account
+//         </a>
+//         <Button className="hidden lg:flex" href="#login">
+//           Sign in
+//         </Button>
+
+//         <Button
+//           className="ml-auto lg:hidden"
+//           px="px-3"
+//           onClick={toggleNavigation}
+//         >
+//           <MenuSvg openNavigation={openNavigation} />
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
